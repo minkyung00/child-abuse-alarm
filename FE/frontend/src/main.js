99213f64ba336b38ac1fbe901b035744097383ea
minkyung00 <mksin00@naver.com>
2022-03-15 23:14:32 +0900

    Fix: eslint 에러 해결
    eslint 에러 무시

--- FE/frontend/src/main.js
+++ FE/frontend/src/main.js
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+import "@/styles/index.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
