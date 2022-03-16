1ff3e21c52a34d6ec92e9fb82bbdf12ed0d19a79
minkyung00 <mksin00@naver.com>
2022-03-16 22:09:56 +0900

    Design: ApplyCenter 디자인

--- FE/frontend/src/views/ApplyCenter.vue
+++ FE/frontend/src/views/ApplyCenter.vue
@@ -1,13 +1,13 @@
 <template>
   <b-container class="apply">
-    <header>
+    <header id="apply-header">
       <h1 class="apply-logo">🌱</h1>
       <h1 class="apply-title">어린이집 등록</h1>
-      <h5 class="apply-desc">최초 등록된 어린이집으로만 이용 가능합니다.</h5>
+      <p class="apply-desc">최초 등록된 어린이집으로만 이용 가능합니다.</p>
     </header>
-
-    <b-form-input type="text" placeholder="어린이집명을 입력해주세요." />
-
+    
+    <b-form-input type="text"
+                  placeholder="어린이집명을 입력해주세요." />
   </b-container>
 </template>
 
@@ -18,5 +18,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.apply {
+  max-width: 800px;
+}
+
+#apply-header {
+  text-align: center;
+  padding: 3rem 0rem;
+
+  .apply-desc {
+    color: $primary-color;
+    font-weight: bold;
+  }
+}
+
+input {
+  padding: 0.75rem 1.5rem;
+  margin-bottom: 20px;
+  border-radius: 30px;
+  border: 2.5px solid $primary-color;
+
+  &:focus {
+    border-color: $primary-color;
+    outline: 0;
+    box-shadow: 0 0 0 0.25rem #33d27b52;
+  }
+}
 
 </style>
