8a1a056d7fd4292b482ffecf9116ddb7d57c9f86
minkyung00 <mksin00@naver.com>
2022-03-16 23:06:26 +0900

    Style: CenterCodeModal 마크업 및 디자인

---
+++ FE/frontend/src/components/CenterCodeModal.vue
@@ -0,0 +1,55 @@
+<template>
+  <transition name="modal" data-backdrop="static">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <form class="modal-container" @submit.prevent="submitForm">
+          <div class="modal-header">
+            <img src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/6562f7bc017800001.png?type=thumb&amp;opt=C72x72" width="36" height="36" alt="" class="ico_cate">
+            <h5>코드 등록</h5>
+            <button
+              type="button"
+              class="btn-close"
+              @click="$emit('close')"
+            ></button>
+          </div>
+
+          <div class="modal-body">
+            <b-form-input type="text" placeholder="코드를 입력하세요"/>
+          </div>
+
+          <div class="modal-footer">
+            <button class="submit-btn"
+                    type="submit"
+            >등록</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "CenterCodeModal",
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-header {
+  img {
+    margin-right: 8px;
+  }
+}
+input {
+  padding: 0.75rem 1.5rem;
+  border-radius: 30px;
+}
+
+h5 {
+  margin: 0;
+}
+
+.submit-btn {
+  width: 5rem;
+}
+</style>
