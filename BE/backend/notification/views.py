84a5cae1bb4ec61eb5e4cb18fe5cc26776632dae
minkyung00 <mksin00@naver.com>
2022-05-09 11:57:46 +0900

    Feat: S3 영상 저장 시 DB 저장 기능 구현
    S3 관련 환경 설정
    S3에 영상이 업로드되면 해당 영상 정보를 DB에 저장하여 웹소켓으로 전송

--- BE/backend/notification/views.py
+++ BE/backend/notification/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.conf import settings
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+import boto3
+
+from .serializers import NotificationSerializer
+from .models import Notification
+
+def get_s3object():
+  s3 = boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
+        region_name = settings.AWS_REGION
+      )
+      
+  obj_list = s3.list_objects(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
+  contents_list = obj_list['Contents']
+  return contents_list
+
+
+def save_notification():
+  contents_list = get_s3object()
+
+  data = {}
+
+  for content in contents_list:
+    key = content['Key']
+    if ('/' not in key):
+      continue
+
+    data['target_center'], data['title'] = key.split('/')
+    data['content'] = 'https://' + settings.AWS_S3_CUSTOM_DOMAIN + '/' + key
+  
+    serializer = NotificationSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+
+class NotificationView(APIView):
+    def get(self, request, id):
+        notifications = get_object_or_404(Notification, id = id)
+        serializer = NotificationSerializer(notifications, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
