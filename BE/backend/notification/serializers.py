e030327946b5ba28de729889d88e744899422964
minkyung00 <mksin00@naver.com>
2022-05-27 14:56:31 +0900

    Change: DB column명 변경
    is_danger, is_warning, is_caution -> status로 변경

--- BE/backend/notification/serializers.py
+++ BE/backend/notification/serializers.py
@@ -5,7 +5,7 @@ from .models import Notification, NotificationWeekly
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id', 'title', 'content', 'hit_count', 'kick_count', 'created_time', 'is_danger', 'is_warning', 'is_caution']
+        fields = ['id', 'title', 'content', 'hit_count', 'kick_count', 'created_time', 'status']
 
 class NotificationWeeklySerializer(serializers.ModelSerializer):
     class Meta:
