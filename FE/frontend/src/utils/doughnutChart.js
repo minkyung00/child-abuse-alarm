b7d4bf2ad556ce060363b0b4e65fd30dc3a384c2
minkyung00 <mksin00@naver.com>
2022-05-29 19:44:16 +0900

    Fix: 차트 렌더링 오류 해결

--- FE/frontend/src/utils/doughnutChart.js
+++ FE/frontend/src/utils/doughnutChart.js
@@ -1,11 +1,38 @@
-import { Chart, registerables } from "chart.js";
-Chart.register(...registerables);
+import { Chart, registerables } from "chart.js"
+Chart.register(...registerables)
+let chart
 
 import { Doughnut } from "vue-chartjs/legacy";
 export default {
   extends: Doughnut,
-  props: ["chartData", "options"],
-  mounted () {
-    this.renderChart(this.chartData, this.options);
+  props: ["chartData", "chartOptions", "weeklyData"],
+  async mounted () {
+    this.$nextTick(function () {
+      this.createChart()
+    })
+  },
+  methods: {
+    createChart() {
+      if (chart !== undefined) {
+        chart.destroy()
+      }
+      chart = new Chart(this.$refs.doughnutChart, {
+        type: 'doughnut',
+        data: this.chartData,
+        options: this.chartOptions
+      })
+      chart.data.datasets[0].data = [
+        this.weeklyData.total_danger,
+        this.weeklyData.total_warning,
+        this.weeklyData.total_caution
+      ]
+      chart.update()
+    },
+  },
+  watch: {
+    weeklyData () {
+      this.createChart()
+    },
+    deep: true
   }
 }
